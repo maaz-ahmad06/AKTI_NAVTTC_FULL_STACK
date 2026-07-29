c335813ed6b004fc7973cb881ae1f94a993b9f56
Maaz Ahmad <maazahmad21988@gmail.com>
2026-07-29 07:37:53 +0500

    Task 4to find your Birthday

--- class-17/Task4.js
+++ class-17/Task4.js
@@ -5,8 +5,12 @@ let today = new Date();
 let currentMonth = today.getMonth() + 1;
 let currentDate = today.getDate();
 
-if(currentMonth > month || (currentMonth === month && currentDate > day)){
+if(currentMonth === month && currentDate === day){
+    console.log("Congratulations! Today is your Birthday.");
+}
+else if(currentMonth > month || (currentMonth === month && currentDate > day)){
     console.log("Wait for next year!");
-}else{
+}
+else{
     console.log("Your birthday is coming soon!");
 }
