35a02eacdcd6275a749818de8c92518f4e15b4fa
Maaz Ahmad <maazahmad21988@gmail.com>
2026-07-29 07:50:35 +0500

    Task 5 to find Age

---
+++ class-17/Task5.js
@@ -0,0 +1,6 @@
+let birthYear = 2004;
+
+let currentYear = new Date().getFullYear();
+
+let age = currentYear - birthYear;
+console.log(`My Birth year is ${birthYear}, and current year is ${currentYear}, so I am ${age} years old`);
