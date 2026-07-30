e2b018490e5c3309a358d83f934b5823fcb65f37
Maaz Ahmad <maazahmad21988@gmail.com>
2026-07-30 15:49:21 +0500

    Class 19 added

---
+++ class-19/script.js
@@ -0,0 +1,29 @@
+// ways to targeting HTML
+
+// document.getElementsByTagName
+// document.getElementsByClassName
+// document.getElementById
+// document.querySelector
+// document.querySelectorAll
+
+// changing Text
+
+// innerHTML
+// outerHTML
+// innerText
+// textContent
+// using these methods we can change the text
+
+// to change styling
+
+// style.color
+// style.backgroud
+// classList
+// classList.add()
+// classList.remove()
+// classList.toggle()
+// classList.contains()
+
+let para = document.getElementById("para");
+para.innerHTML  = "This is para";
+para.style.color = "red";
