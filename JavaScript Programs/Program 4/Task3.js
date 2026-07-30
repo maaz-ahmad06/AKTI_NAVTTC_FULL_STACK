86f0d8d3af0fa973496b6295e30bf25f576ec4b0
Maaz Ahmad <maazahmad21988@gmail.com>
2026-07-30 22:09:51 +0500

    Task 3 change Text color using three different buttons and colors

---
+++ JavaScript Programs/Program 4/Task3.js
@@ -0,0 +1,11 @@
+let h1 = document.getElementById("h1")
+
+function red(){
+    h1.style.color = "red";
+}
+function blue(){
+    h1.style.color = "blue";
+}
+function green(){
+    h1.style.color = "green";
+}
