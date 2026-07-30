f774018faf7632500b443cc02e9c1dbdcb14e26e
Maaz Ahmad <maazahmad21988@gmail.com>
2026-07-30 13:32:50 +0500

    JavaScript Fuctions added

---
+++ class-18/functions.js
@@ -0,0 +1,23 @@
+// Types of functions
+
+// Anonymous fucntion => without a name functions is Anonymous function
+// Named fucntion =>
+// Parameterized function
+// Two Parameterized function
+// Function Expression => when we put a function in a variable
+// IIFE (Immediatly Invoked function Expression)
+// Arrow function
+// Return Value
+
+// function squire ( a ){
+//     return a**2;
+// }
+// console.log(squire(7));
+
+// let a = (num) => {
+//     if(num % 2 == 0){
+//         return "even";
+//     }
+//     return "odd"
+// }
+// console.log(a(9));
