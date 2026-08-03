53cbd4a2f5b39ac7602f390f6c551de053916d48
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-04 03:43:51 +0500

    Class 21 | Targeting spacific keys

--- class-21/Tasks/Task1.js
+++ class-21/Tasks/Task1.js
@@ -1,3 +1,4 @@
+// Q1
 let student = {
     name: "maaz",
     age: 22,
@@ -5,3 +6,13 @@ let student = {
     institute: "ICP"
 }
 console.log(student);
+
+// Q2
+let mobile = {
+    brand: "Samsung",
+    model: "Galaxy S21",
+    price: 799,
+    color: "Phantom Gray"
+}
+console.log(mobile.brand);
+console.log(mobile.price);
