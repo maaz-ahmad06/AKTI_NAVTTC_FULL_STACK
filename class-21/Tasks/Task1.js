2c200fdd8e0b3fb5b1b99b3c0d659e81c2f49bae
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-04 21:17:25 +0500

    Task1 modified

--- class-21/Tasks/Task1.js
+++ class-21/Tasks/Task1.js
@@ -1,4 +1,3 @@
-// Q1
 let student = {
     name: "maaz",
     age: 22,
@@ -6,13 +5,3 @@ let student = {
     institute: "ICP"
 }
 console.log(student);
-
-// Q2
-let mobile = {
-    brand: "Samsung",
-    model: "Galaxy S21",
-    price: 799,
-    color: "Phantom Gray"
-}
-console.log(mobile.brand);
-console.log(mobile.price);
