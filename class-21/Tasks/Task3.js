0ff8116c99382fe794382beb423ccf1570d8077a
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-04 21:18:44 +0500

    Class 21 | Task3 added

---
+++ class-21/Tasks/Task3.js
@@ -0,0 +1,12 @@
+// Q3
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2021,
+    color: "Silver"
+}
+
+car.owner = "Ali";
+car.color = "White";
+delete car.model;
+console.log(car);
