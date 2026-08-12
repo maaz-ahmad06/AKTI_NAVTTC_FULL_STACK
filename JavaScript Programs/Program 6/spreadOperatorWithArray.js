2c4d0cc7916d576fa625d832c73b006fcb66138e
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-12 18:38:43 +0500

    Spread Operator with Array & Object

---
+++ JavaScript Programs/Program 6/spreadOperatorWithArray.js
@@ -0,0 +1,5 @@
+const frontend = ["HTML", "CSS", "JavaScript"];
+const backend = ["Node.js", "Express", "MongoDB"];
+
+const fullStack = [...frontend, ...backend];
+console.log(fullStack);
---
+++ JavaScript Programs/Program 6/spreadOperatorWithObject.js
@@ -0,0 +1,9 @@
+const user = {
+name: "Ahmed",
+age: 22
+}
+const newObject = {
+    ...user,
+    city: "peshawar"
+}
+console.log(newObject);
