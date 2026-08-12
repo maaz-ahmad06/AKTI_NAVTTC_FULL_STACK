4330a1a48d2f320bcff2ea7ef381d9a8550bbfd6
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-12 19:59:22 +0500

    Class 24

---
+++ class-24/script.js
@@ -0,0 +1,15 @@
+// var 
+// let
+// const
+// Reference Error
+// Type Error
+// syntix Error
+// hoisting => js code run karne se pehle variable aur function decleration ko top of scope leke jati ha
+// Temporal Dead zone => let aur const variable ko declare karne se pehle access karne ka time TDZ kehlata ha
+// callback function => ek function ko doosre function ke argument ke taur pa pass karna callback function kehlata ha
+// Higher ordre function => jo func kisi func ko argument me leta hai ya func return karta ha
+// closure fuction => closure tab hota ha jab inner func apne outer func ke variable ko yaad rakhta hai even after outer func finish ho jaye
+// Local Storage => browser me data long-term save karne ke liye use hota ha
+// kookies => cookies browser me small piece of data store karti ha aur automatically server ko requist ke saath bheji ja sakti ha
+// sessions
+// Firebase => ke founders James Tamplin aur Andrew Lee
