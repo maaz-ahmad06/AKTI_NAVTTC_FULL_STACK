e88802b0e24eb4e7062fa5838dd3381a6a0abb1f
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-12 18:37:33 +0500

    Array & Object Destructuring

---
+++ JavaScript Programs/Program 6/arrayDestructuring.js
@@ -0,0 +1,4 @@
+const subjects = ["HTML", "CSS", "JavaScript", "React"];
+const [firstSubject, secondSubject, thirdSubject] = subjects;
+
+console.log(firstSubject, secondSubject, thirdSubject);
---
+++ JavaScript Programs/Program 6/objectDestructuring.js
@@ -0,0 +1,8 @@
+const student = {
+name: "Ali",
+age: 21,
+course: "MERN",
+city: "Peshawar"
+}
+const { name, age, course } = student;
+console.log(name, age, course);
