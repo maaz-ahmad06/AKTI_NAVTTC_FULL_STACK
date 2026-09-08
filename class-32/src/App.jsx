52a31552835aee16393896ff06109f7aa3019e5c
Maaz Ahmad <maazahmad21988@gmail.com>
2026-09-08 15:36:58 +0500

    Class 32/33 | server connection

---
+++ class-32/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-4xl font-bold text-blue-500 bg-amber-500'>
+      hello shadeCN materialUI 
+    </div>
+  )
+}
+
+export default App
